dfb5e480b3109c5e47c84668b7f6c5223dc3febe
Lloyd Nyarugwe <llorugwe@gmail.com>
2024-02-01 02:18:51 -0800

    My fifth commit

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * func_stack - ...
+ * @head: This is the stack's head
+ * @line_number: line_number
+ * Return: nothing
+*/
+void func_stack(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+	bus.lifi = 0;
+}
---
+++ swaping.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * func_swap - ...
+ * @head: This is the stack's head
+ * @line_number: line_number
+ * Return: nothing
+*/
+void func_swap(stack_t **head, unsigned int line_number)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n;
+	h->n = h->next->n;
+	h->next->n = aux;
+}
