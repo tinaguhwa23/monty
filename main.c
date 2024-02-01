3f40bbd2fa9c9a8cde2bfa146cc57511c626a54a
tinaguhwa23 <tinaguhwa@gmail.com>
2024-02-01 01:52:19 -0800

    My second commit

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+#include <stdio.h>
+
+#define MAX_LINE_LENGTH 1024
+
+bus_t bus = {NULL, NULL, NULL, 0};
+/**
+* main - This is a monty code interpreter
+* @argc: Is the number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	char content[MAX_LINE_LENGTH];
+	FILE *file;
+	stack_t *stack = NULL;
+	unsigned int line_number = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	bus.file = file;
+
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(content, sizeof(content), file) != NULL)
+	{
+		line_number++;
+		bus.content = content;
+
+		execute(content, &stack, line_number, file);
+	}
+
+	free_stack(stack);
+	fclose(file);
+	return (0);
+}
